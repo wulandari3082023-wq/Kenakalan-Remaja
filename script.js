e6d7bdbe357e0542260afaf541272f85e405565f
wulandari3082023-wq <wulandari3082023@gmail.com>
2025-12-17 20:35:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+function login() {
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+
+    if (user === "admin" && pass === "remaja123") {
+        document.getElementById("loginBox").style.display = "none";
+        document.getElementById("content").style.display = "block";
+    } else {
+        document.getElementById("loginMessage").innerText =
+            "Username atau password salah!";
+    }
+}
+
+function logout() {
+    document.getElementById("content").style.display = "none";
+    document.getElementById("loginBox").style.display = "block";
+}
+
 function tampilkanDampak() {
-    document.getElementById("hasil").innerHTML =
-        "Kenakalan remaja dapat menyebabkan rusaknya masa depan, masalah hukum, "
-        + "gangguan kesehatan mental, dan konflik sosial.";
+    document.getElementById("hasil").innerText =
+        "Kenakalan remaja dapat menyebabkan masalah hukum, "
+        + "gangguan mental, dan masa depan yang terhambat.";
 }
