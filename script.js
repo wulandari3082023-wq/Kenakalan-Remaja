17c8932741fc48097c6b212a63a035bdef7781e4
wulandari3082023-wq <wulandari3082023@gmail.com>
2025-12-17 20:22:32 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function tampilkanDampak() {
+    document.getElementById("hasil").innerHTML =
+        "Kenakalan remaja dapat menyebabkan rusaknya masa depan, masalah hukum, "
+        + "gangguan kesehatan mental, dan konflik sosial.";
+}
